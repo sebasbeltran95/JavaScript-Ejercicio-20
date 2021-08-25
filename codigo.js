f1dc0088cb9ea7e572bde0a7427a5ea28153494d
sebastian beltran zapata <sebastian199502@hotmail.com>
2021-08-24 20:39:31 -0500

    JavaScript-Ejercicio-20

---
+++ codigo.js
@@ -0,0 +1,10 @@
+let numero3 = Number(prompt("Introduce un numero"));
+let numero4 = Number(prompt("Introduce otro numero"));
+let numero5 = Number(prompt("Introduce otro numero"));
+
+function sumaConEsteroides(numero1, numero2, numero3) {
+    let operacion = (numero1 + numero2) * numero3;
+    console.log(operacion);
+}
+
+sumaConEsteroides(numero3, numero4, numero5);
